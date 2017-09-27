d644e5e4eca54f87c084bb4300d105cc2c4861af
iamDio <claudio.villani00@gmail.com>
2017-09-27 18:09:29 -0400

    added jakes card gallery modifications

--- galactic_game/src/containers/container_CardDetail.js
+++ galactic_game/src/containers/container_CardDetail.js
@@ -24,10 +24,12 @@ class CardDetail extends Component {
 
             
             
-
             <div className="card-image">
-                <img height="500px" alt="" src={ this.props.card.selectCard.image }></img> 
-                <h2 className='card-detail-card-name'>{this.props.card.selectCard.name}<br />Damage: {this.props.card.selectCard.damage}</h2>
+                <h1 className="col-xs-2"></h1>
+                <img className='col-xs-6 displayImgDawg' height="500px" alt="" src={ this.props.card.selectCard.image }></img> 
+                <h1 className="col-xs-1"></h1>
+                <h1 className='col-xs-3 card-detail-card-name'>{this.props.card.selectCard.name}<br/><br/>Health: {this.props.card.selectCard.health}<br/><br/>Damage: {this.props.card.selectCard.damage}<br/><br/> {this.props.card.selectCard.description}</h1>
+                <h1 className="col-xs-1"></h1>
                 
             </div>
             
